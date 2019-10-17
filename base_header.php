46a8ff1214a031823214031ce9eb6b7c432797ed
juliobissoli <49066297+juliobissoli@users.noreply.github.com>
2019-10-17 07:51:38 -0300

    Ícones  inseridos pelo repositório web (fontawesome). Baixar pacote e descementar a linha 10 desse arquivo

--- base_header.php
+++ base_header.php
@@ -6,7 +6,7 @@
 
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
 <script type="text/javascript" src="/js/jquery-3.1.0.min.js"></script>
-<!-- <script src="https://kit.fontawesome.com/d9c369bd27.js"></script>Link para servidor de icones  -->
-<link href="/css/icones/css/all.css" rel="stylesheet">
+<script src="https://kit.fontawesome.com/d9c369bd27.js"></script>Link para servidor de icones 
+<!-- <link href="/css/icones/css/all.css" rel="stylesheet"> -->
 
   <script type="text/javascript" src="/js/base.js"></script>
